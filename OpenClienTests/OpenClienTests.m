dd2e6ea6691a5d75a733506858d7e8b422308228
Changbeom Ahn <kewlbear@gmail.com>
2014-06-11 17:17:52 +0900

    테스트 추가

--- OpenClienTests/OpenClienTests.m
+++ OpenClienTests/OpenClienTests.m
@@ -2,11 +2,23 @@
 //  OpenClienTests.m
 //  OpenClienTests
 //
-//  Created by 안창범 on 2014. 5. 9..
-//  Copyright (c) 2014년 안창범. All rights reserved.
+// Copyright 2014 Changbeom Ahn
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #import <XCTest/XCTest.h>
+#import "OpenClien.h"
 
 @interface OpenClienTests : XCTestCase
 
@@ -26,9 +38,15 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testParsingArticleWithFileAndLink
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    OCArticleParser *parser = [[OCArticleParser alloc] init];
+    NSURL *URL = [NSURL URLWithString:@"http://www.clien.net/cs2/bbs/board.php?bo_table=pds&wr_id=123454"];
+    NSData *data = [NSData dataWithContentsOfURL:URL];
+    OCArticle *article = [URL article];
+    [parser parse:data article:article];
+    XCTAssertGreaterThan([parser.files count], 0);
+    XCTAssertGreaterThan([parser.links count], 0);
 }
 
 @end
